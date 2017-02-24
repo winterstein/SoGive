3506bbcd149424dcce4939d3f8215028a11b5415
Daniel Appel <daniel.appel.winterwell@gmail.com>
2017-02-24 13:23:26 +0000

    updated the footer on the template, added the FAQ page, added JS to govern the accordion-like-function of the FAQ page, and added CSS to govern the FAQ page

---
+++ pages/faq.md
@@ -0,0 +1,23 @@
+bgclass: faq-bg
+
+<div class="faq-bg">
+</div>
+
+<div class="col-md-12">
+	<div class="col-md-offset-3 col-md-6">
+		<div id="faqsection">
+			<button class="accordion btn-lg black-font btn-accordion">What is SoGive?<span class="pull-right glyphicon glyphicon-plus"></span></button>
+			<div class="panel">
+				<p class="font-18 black-font">SoGive is a tool that lets you see the impact you’ve made in the world through all your charitable giving. Sign up today to make the world a better place.</p>
+			</div>
+			<!-- Cheap vertical space, you can increase or decrease it by changing the em level-->
+			<div class="col-xs-12" style="height:1em;">
+			</div>
+			<!-- End of cheap vertical space -->
+			<button class="accordion btn-lg black-font btn-accordion">Why should I use SoGive?<span class="pull-right glyphicon glyphicon-plus"></span></button>
+			<div class="panel">
+				<p class="font-18 black-font">text that sits in place until better text is given</p>
+			</div>
+		</div>
+	</div>
+</div>
--- webroot/js/sogive.js
+++ webroot/js/sogive.js
@@ -94,3 +94,41 @@ $(document).ready(function() {
     	parallax();
 	});
 });
+
+$(document).ready(function() {
+	var jumboHeight = $('.jumbotron').outerHeight();
+	function parallax(){
+    	var scrolled = $(window).scrollTop();
+    	$('.faq-bg').css('height', (jumboHeight-scrolled) + 'px');
+	}
+
+	$(window).scroll(function(e){
+    	parallax();
+	});
+});
+
+// The Accordion Function
+$(document).ready(function () {
+var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
+var i;
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+    	var setClasses = !this.classList.contains('active');
+    		setClass(acc, 'active', 'remove');
+    		setClass(panel, 'show', 'remove');
+
+    		if (setClasses) {
+ 	    	   this.classList.toggle("active");
+ 	    	   this.nextElementSibling.classList.toggle("show");
+  			}
+  	}
+}
+
+function setClass(els, className, fnName) {
+	for (var i = 0; i < els.length; i++) {
+   	    els[i].classList[fnName](className);
+   	}
+}
+});
