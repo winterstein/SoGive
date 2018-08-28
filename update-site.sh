c7fc6d9d9c2f4c8964a8a45f00e65bd75d5c40d8
DA <daniel.appel.winterwell@gmail.com>
2018-08-28 11:33:28 +0100

    updated the update-site.sh script to use the new Jerbil changes

--- update-site.sh
+++ update-site.sh
@@ -6,34 +6,26 @@ SSHCOMMAND="ssh winterwell@$TARGET"
 TARGETDIR='/home/winterwell/SoGive'
 GITSHORTHAND="git --git-dir=$TARGETDIR/.git/ --work-tree=$TARGETDIR"
 
-echo -e "Telling the server to update it's $PROJECT git repo"
-echo -e "> Garbage Collecting..."
+printf "\nTelling the server to update it's $PROJECT git repo\n"
+printf "\t> Garbage Collecting...\n"
 $SSHCOMMAND "$GITSHORTHAND gc --prune=now"
-echo -e "> Pulling Origin..."
+printf "\t> Pulling Origin...\n"
 $SSHCOMMAND "$GITSHORTHAND pull origin master"
-echo -e "> Resetting Files to version held on github"
+printf "\t> Resetting Files to version held on github\n"
 $SSHCOMMAND "$GITSHORTHAND reset --hard FETCH_HEAD"
-echo -e ""
 
-echo -e "> Optimising Images..."
+printf "\t> Optimising Images...\n"
 $SSHCOMMAND "optipng $TARGETDIR/webroot/img/*.png"
 $SSHCOMMAND "jpegoptim $TARGETDIR/webroot/img/*.jpg"
 $SSHCOMMAND "jpegoptim $TARGETDIR/webroot/img/*.jpeg"
-echo -e ""
 
-echo -e "> Setting permissions on the image files"
+printf "\t> Setting permissions on the image files\n"
 $SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.png"
 $SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.jpg"
 $SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.jpeg"
-echo -e ""
 
-echo -e "> Converting Markdown to HTML..."
-$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil gc --prune=now'
-$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil pull origin master'
-$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil reset --hard FETCH_HEAD'
-rsync -hp ~/winterwell/code/middleware/xstream/xstream.jar winterwell@$TARGET:/home/winterwell/jerbil/lib/
-rsync -hP ~/winterwell/code/middleware/xstream/xmlpull-1.1.3.1.jar winterwell@$TARGET:/home/winterwell/jerbil/lib/
-$SSHCOMMAND "cd /home/winterwell/jerbil/ && java -cp jerbil.jar:lib/* Jerbil $TARGETDIR"
-echo -e ""
+printf "\y> Converting Markdown to HTML...\n"
+$SSHCOMMAND 'wget -cO - "https://www.winterwell.com/software/downloads/jerbil-all.jar" >> /home/winterwell/jerbil/jerbil-all.jar'
+$SSHCOMMAND "cd /home/winterwell/jerbil/ && java -cp jerbil-all.jar Jerbil $TARGETDIR"
 
-echo -e "$PROJECT Website has now been updated"
+printf "\n$PROJECT Website has now been updated\n"
